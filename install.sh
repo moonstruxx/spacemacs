fb883f832e37df0aa1af6be32b8f9a7bc9639723
björn thorwirth <moonstruxx@gmx.de>
2018-11-12 05:16:29 +0100

    copy tt fonts into place and git hint for path setup

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
 ln -s 3rd-party/spacemacs ~/.emacs.d
 #add install of socket activated rtags
 ln -s ./ ~/.spacemacs
+mkdir ~/.fonts
+cp misc/fonts/* ~/.fonts
+fc-cache -f -v
+echo "please copy 3rdparty/fasd into you path or add that dir to $PATH in your .bashrc"
